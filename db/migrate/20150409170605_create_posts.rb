0cf97456292931ffa7f867eaa0adc45d99e3c8c0
laura butler <laura.robyn.butler@gmail.com>
2015-04-09 21:52:43 +0000

    to to user

---
+++ db/migrate/20150409170605_create_posts.rb
@@ -0,0 +1,11 @@
+cd class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :link
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
