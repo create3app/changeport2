795adfe3c0aa7fc21f9392441f704bc972afda53
Tom Dallimore <me@tomdallimore.com>
2015-05-07 00:35:49 +0700

    Set up flash logic for displaying controller action messages.

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,14 +3,14 @@ class FollowsController < ApplicationController
     def create
         set_user
         current_user.followee_users.build(followee_id: @user.id).save!
-        flash[:success] = "You have successfully followed user #{@user.name}."
+        flash[:success] = "You have successfully followed #{@user.name}."
         redirect_to user_url(current_user)
     end
 
     def destroy
         set_user
         current_user.followees.delete(@user)
-        flash[:success] = "You have sucessfully removed #{@user.name} from your follow list."
+        flash[:success] = "You have sucessfully unfollowed #{@user.name}."
         redirect_to user_url(current_user)
     end
 
